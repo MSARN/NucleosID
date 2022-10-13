b54ce7c53620e6dc18c3eff9296d58a17e76ab99
Jérôme Pansanel <jerome.pansanel@iphc.cnrs.fr>
2022-10-13 20:58:51 +0200

    Add missing filter

--- nucleosid/mgf_data_analyzer.py
+++ nucleosid/mgf_data_analyzer.py
@@ -24,7 +24,11 @@ class MGFDataAnalyzer(object):
         """Initialize the MGFDataAnalyszer class."""
         self.exclusion_time = analysis_parameters['exclusion_time']
         self.ms_tolerance = analysis_parameters['ms_tolerance']
+        self.ms_tolerance_type = analysis_parameters['ms_tolerance_type']
+        self.ms_ms_tolerance_type = analysis_parameters['ms_ms_tolerance_type']
         self.ms_ms_tolerance = analysis_parameters['ms_ms_tolerance']
+        self.ms_ms_score_threshold = analysis_parameters['ms_ms_score_threshold']
+        self.exclusion_time = analysis_parameters['exclusion_time']
         """Initialize the SpectrumAnalyzer class."""
         self.ms_ms_spectra = ms_ms_spectra
         self.arn_modifications = arn_modifications
@@ -34,44 +38,93 @@ class MGFDataAnalyzer(object):
             'Theoretical MS (Da)': pd.Series(dtype='float'),
             'Observed MS/MS (Da)': pd.Series(dtype='float'),
             'Theoretical MS/MS (Da)': pd.Series(dtype='float'),
-            'Relative intensity (%)': pd.Series(dtype='float'),
-            'Start analysis (s)': pd.Series(dtype='float')
+            'Score (%)': pd.Series(dtype='float'),
+            'Detection time (s)': pd.Series(dtype='float')
         })
 
     def find_arn_modifications(self):
         """Analyse ARN modifications in the spectrum."""
+        matching_modifications = []
         for spectrum in self.ms_ms_spectra:
             exact_mass = spectrum.get_exact_mass()
-            # Don't check time now
-            # time = spectrum.get_time()
-            # Si la masse est celle de la modif, alors
             for modification_name in self.arn_modifications:
-                hit_found = False
                 modified_mass = self.arn_modifications[modification_name]['ms_value']
-                delta = abs(
-                    exact_mass - modified_mass
-                )
+                if self.ms_tolerance_type == 'ppm':
+                    delta = abs(exact_mass - modified_mass)/modified_mass * 1000000
+                else:
+                    delta = abs(exact_mass - modified_mass)
                 if delta <= self.ms_tolerance:
+                    # A matching mass has been found
                     peaks = spectrum.get_peaks()
-                    matching_peaks = []
+                    matching_peaks = {}
                     modified_frag_masses = self.arn_modifications[modification_name]['ms_ms_values']
-                    for modified_frag_mass in modified_frag_masses:
-                        for (frag_mass, intensity) in peaks:
-                            diff = abs(frag_mass - float(modified_frag_mass))
+                    max_intensity = 0
+                    # Max intensity for the current modification matching a fragment
+                    # spectrum
+                    mfrag_intensity = 0
+                    for (frag_mass, intensity) in peaks:
+                        max_intensity = max(max_intensity, intensity)
+                        for modified_frag_mass in modified_frag_masses:
+                            if self.ms_ms_tolerance_type == 'ppm':
+                                diff = abs(frag_mass - modified_frag_mass) / modified_frag_mass * 1000000
+                            else:
+                                diff = abs(frag_mass - modified_frag_mass)
                             if diff <= self.ms_ms_tolerance:
-                                matching_peaks.append(round(frag_mass))
-                                hit_found = True
-                    if hit_found:
-                        # Add the missing analysis value
-                        self.arn_analysis.loc[len(self.arn_analysis.index)] = [
-                            modification_name,
-                            exact_mass,
-                            modified_mass,
-                            ';'.join([str(x) for x in matching_peaks]),
-                            ';'.join([str(y) for y in modified_frag_masses]),
-                            0,
-                            0
-                        ]
+                                mfrag_intensity = max(intensity, mfrag_intensity)
+                                if modified_frag_mass not in matching_peaks:
+                                    # We need to register intensity as we only
+                                    # store the frag_mass with the higher
+                                    # intensity corresponding to a
+                                    # modified_frag_mass
+                                    matching_peaks[modified_frag_mass] = (
+                                        frag_mass, intensity
+                                    )
+                                else:
+                                    # A peak for this fragment has already
+                                    # been found
+                                    if intensity > matching_peaks[modified_frag_mass][1]:
+                                        matching_peaks[modified_frag_mass] = (
+                                            frag_mass, intensity
+                                        )
+                    if matching_peaks:
+                        frag_max_intensity = 0
+                        frag_masses = []
+                        # Post analysis
+                        for key_mass in matching_peaks:
+                            frag_max_intensity = max(
+                                matching_peaks[key_mass][1],
+                                frag_max_intensity
+                            )
+                            frag_masses.append(matching_peaks[key_mass][0])
+
+                        if max_intensity == 0:
+                            score = 0
+                        else:
+                            score = frag_max_intensity / max_intensity
+                        if (score * 100) > self.ms_ms_score_threshold:
+                            matching_masses = ';'.join([str(x) for x in frag_masses])
+                            mod_frag_masses = ';'.join([str(y) for y in modified_frag_masses])
+                            matching_modifications.append({
+                                'mod_name': modification_name,
+                                'exact_mass': exact_mass,
+                                'modified_mass': modified_mass,
+                                'matching_masses': matching_masses,
+                                'mod_frag_masses': mod_frag_masses,
+                                'score': round(score*100, 2),
+                                'rtinseconds': spectrum.get_rtinseconds()
+                            })
+        # Only modification where ms ms match is added!
+        for match in matching_modifications:
+            # Add the missing analysis value
+            self.arn_analysis.loc[len(self.arn_analysis.index)] = [
+                match['mod_name'],
+                match['exact_mass'],
+                match['modified_mass'],
+                match['matching_masses'],
+                match['mod_frag_masses'],
+                match['score'],
+                match['rtinseconds'],
+            ]
 
     def get_analysis(self):
         """Return the result of ARN modification analysis."""
