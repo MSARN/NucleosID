25f98bde6e5b766cb697a77090a5b5eae95b1ddb
Jérôme Pansanel <jerome.pansanel@iphc.cnrs.fr>
2024-10-19 21:59:19 +0200

    New test for analysis_writer

---
+++ tests/unit/test_analysis_writer.py
@@ -0,0 +1,42 @@
+# Copyright 2024 CNRS and University of Strasbourg
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Analysis writer test class."""
+
+import pandas as pd
+import pytest
+
+from nucleosid import analysis_writer
+
+
+@pytest.fixture(scope="session")
+def test_write_analysis():
+    """Test the write_analysis function."""
+    data = pd.DataFrame({
+        'Modification': pd.Series(dtype='str'),
+        'Observed MS (Da)': pd.Series(dtype='float'),
+        'Theoretical MS (Da)': pd.Series(dtype='float'),
+        'Observed MS/MS (Da)': pd.Series(dtype='float'),
+        'Theoretical MS/MS (Da)': pd.Series(dtype='float'),
+        'Score (%)': pd.Series(dtype='float'),
+        'Detection time (s)': pd.Series(dtype='float')
+    })
+
+    writer = analysis_writer.AnalysisWriter(data)
+    output_file = tmp_factory.mktemp("data").join("test.csv")
+    writer.write(output_file)
+    assert os.path.isfile(output_file) == True
+    output_file = tmp_factory.mktemp("data").join("test.xlsx")
+    writer.write(output_file)
+    assert os.path.isfile(output_file) == True
